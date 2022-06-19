c6849ae5323cf6794a1e2f5a15e7842abc232fed
justinkirk8 <96632211+justinkirk8@users.noreply.github.com>
2022-06-19 12:38:54 -0400

    add deliverable 2 code

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -13,6 +13,11 @@ summary(MechaCar_mpg_lm)
 
 #Deliverable 2
 
+Suspension_coil <- read.csv(file="Resources/Suspension_Coil.csv")
+
+total_summary <- Suspension_coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+lot_summary <- Suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
 
 
 
